c0a9a03d47e74283819515ed7d7869d3c0150e6b
Erno Chen <chenerno@gmail.com>
2017-09-24 09:04:04 -0700

    Update app.js
    Added collecting, restricted kingdom names to alphanumeric characters, automatic capitalizing for the kingdom name.

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const sql = require("sqlite");
 var deleteIds = [];
-sql.open("./kingdoms.sqlite");
+sql.open("./kingdoms.sqlite").then(() => sql.run("CREATE TABLE IF NOT EXISTS timedEvents (userId TEXT, serverId TEXT, eventCode INTEGER, dateStarted TEXT)"));
 function genLP () {
-	var returnString = "ccc00000|ccc00000|000kkk00|000kkk00|000kkk00|00000000|ttt00000|ttt00000";
+	var returnString = "00000ccc|00000ccc|000kkk00|000kkk00|000kkk00|00000000|ttt00000|ttt00000";
 	for(var i = 0; i < returnString.length; i++) {
 		if(returnString[i] == 0) {
 			let num = Math.random();
@@ -22,7 +22,7 @@ function unencrypt (code) {
 	var returnString = "";
 	for(var i = 0; i < code.length; i++) {
 		let current;
-		switch(code[i]) { //This supposedly works on Discord?
+		switch(code[i]) {
 			case "c":
 				current = "馃彮";
 				break;
@@ -55,6 +55,10 @@ client.on('ready', () => {
 });
 client.on('message', msg => {
 	if(msg.author.bot) return;
+	if(!msg.guild) {
+		msg.reply("I'm sorry, I can't receive commands in DMs.");
+		return;
+	}
 	if(msg.content.trim() == '=help') {
 		var theEmbed = new Discord.RichEmbed();
 		theEmbed.setTitle("Commands");
@@ -64,9 +68,11 @@ client.on('message', msg => {
 		theEmbed.addField("=addk [name]", "Adds a kingdom to the database named [name]. One per person per server only.");
 		theEmbed.addField("=renamek [name]", "Renames your kingdom to [name]."); 
 		theEmbed.addField("=delk", "Deletes your kingdom. Needs confirmation."); 
-		theEmbed.addField("=kingdominfo", "Gives info on your kingdom.")
-		theEmbed.addField("=clear [num] (user)", "Clears [num] messages. If (user) is specified, clears all messages from (user) in a search of [num] messages. [num] must be 1-100.");
+		theEmbed.addField("=kingdominfo (DM)", "Gives info on your kingdom. If (DM) is \"DM\", then the bot will send you the info in DM (if you allow it to.)")
+		theEmbed.addField("=clear [num] (user)", "Clears [num] messages. If (user) is specified, clears all messages from (user) in a search of [num] messages. [num] must be 1-99.");
 		theEmbed.addField("=credits", "Shows credits. What did you expect?")
+		theEmbed.addField("=land (DM)", "Shows your land plot. If (DM) is \"DM\", then the bot will send you the land plot in DM (if you allow it to.)")
+		theEmbed.addField("=collect", "Collects any copper coins avaliable. Up to 60 copper coins. You get 1 copper coin every 24 minutes.");
 		theEmbed.setColor(0x7289da);
 		theEmbed.setFooter("Requested by " + msg.author.username + "#" + msg.author.discriminator, msg.author.avatarURL);
 		theEmbed.setTimestamp();
@@ -175,8 +181,8 @@ client.on('message', msg => {
 	if(msg.content.trim() == '=invite') {
 		msg.channel.send("http://bit.ly/kingbot");
 	}
-	if(msg.content.trim() == '=leave') { //If for some reason, I need to make a bot leave a server.
-		if(msg.author.id != 298636036135714816) {
+	if(msg.content.trim() == '=leave') {
+		if(msg.author.id != 298636036135714816 || msg.user.id != msg.guild.ownerID) {
 			return;
 		}
 		msg.channel.send("Kthx, goodbye!").then(() => msg.guild.leave());
@@ -234,8 +240,13 @@ client.on('message', msg => {
 					else if(/<@\d*|@everyone>/.test(content)){
 						msg.reply("No pings allowed in your kingdom name!");
 					}
+					else if(!(/^[a-zA-z\d\s]+$/.test(content))) {
+						msg.reply("Alphanumeric characters only, please.");
+					}
 					else if(!row) { // Can't find the row.
-						sql.run("INSERT INTO kingdoms VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [msg.author.id, content, 1, 1, null, msg.guild.id, 0, 0, 0, 0, 0, genLP()]);
+						content = content.toLowerCase().replace(/\b\w/g, a => a.toUpperCase());
+						sql.run("INSERT INTO kingdoms VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [msg.author.id.toString(), content, null, msg.guild.id.toString(), 0, 0, 0, 0, 0, genLP()]);
+						sql.run("INSERT INTO timedEvents VALUES (?, ?, ?, ?)", [msg.author.id.toString(), msg.guild.id.toString(), 333, new Date().valueOf().toString()]);
 						msg.reply("Kingdom " + content + " added!");
 					}
 					else { // Can find the row.
@@ -244,10 +255,12 @@ client.on('message', msg => {
 				}
 			});
 		}).catch(() => {
-			sql.run("CREATE TABLE IF NOT EXISTS kingdoms (userId INTEGER, name TEXT, aa INTEGER, da INTEGER, attackDate TEXT, serverId INTEGER, wood INTEGER, iron INTEGER, diamond INTEGER, kingCoins INTEGER, copperCoins INTEGER, land TEXT)").then(() => {
-				sql.run("INSERT INTO kingdoms VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [msg.author.id, content, 1, 1, null, msg.guild.id, 0, 0, 0, 0, 0, genLP()]);
+			sql.run("CREATE TABLE IF NOT EXISTS kingdoms (userId TEXT, name TEXT, attackDate TEXT, serverId TEXT, wood INTEGER, iron INTEGER, diamond INTEGER, kingCoins INTEGER, copperCoins INTEGER, land TEXT)").then(() => {
+				content = content.toLowerCase().replace(/\b\w/g, a => a.toUpperCase());
+				sql.run("INSERT INTO kingdoms VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [msg.author.id.toString(), content, null, msg.guild.id.toString(), 0, 0, 0, 0, 0, genLP()]);
+				sql.run("INSERT INTO timedEvents VALUES (?, ?, ?, ?)", [msg.author.id.toString(), msg.guild.id.toString(), 333, new Date().valueOf().toString()]);
 				msg.reply("Kingdom " + content + " added! (In accordance with the Discord Developer ToS, by adding your kindgom you agree to let KingdomBot store End User Data. If you do not agree, use =delk.)");
-			})
+			});
 		});
 	}
 	if(msg.content.trim().startsWith('=renamek ')) {
@@ -277,8 +290,12 @@ client.on('message', msg => {
 					else if(/<@\d*|@everyone>/.test(content)){
 						msg.reply("No pings allowed in your kingdom name!");
 					}
+					else if(!(/^[a-zA-z\d\s]+$/.test(content))) {
+						msg.reply("Alphanumeric characters only, please.");
+					}
 					else { // Can find the row.
-						sql.run('UPDATE kingdoms SET name = ? WHERE userId = ? AND serverId = ?', [content, msg.author.id, msg.guild.id]);
+						content = content.toLowerCase().replace(/\b\w/g, a => a.toUpperCase());
+						sql.run('UPDATE kingdoms SET name = ? WHERE userId = ? AND serverId = ?', [content, msg.author.id.toString(), msg.guild.id.toString()]);
 						msg.reply("Your kingdom was renamed " + content + "!");
 					}
 				}
@@ -289,24 +306,63 @@ client.on('message', msg => {
 	}
 	if(msg.content.trim().startsWith("=kingdominfo")) {
 		let content = Object.create(msg).content.trim();
-		if(content === "=kingdominfo") {
-			sql.get('SELECT * FROM kingdoms WHERE userId = ? AND serverId = ?', [msg.author.id, msg.guild.id]).then(row => {
-				if(!row) {
-					msg.reply("Couldn't find your kingdom...");
+		sql.get('SELECT * FROM kingdoms WHERE userId = ? AND serverId = ?', [msg.author.id.toString(), msg.guild.id.toString()]).then(row => {
+			if(!row) {
+				msg.reply("Couldn't find your kingdom...");
+				return;
+			}
+			var theEmbed = new Discord.RichEmbed();
+			theEmbed.setFooter("Requested by " + msg.author.username + "#" + msg.author.discriminator, msg.author.avatarURL);
+			theEmbed.setTitle("Info for Kingdom " + row.name);
+			theEmbed.setDescription(`<:kingCoin:360975393756545024> - ${row.kingCoins}\n<:copperCoin:361226469336416276> - ${row.copperCoins}\n<:wood:356908039951089674> - ${row.wood}\n<:iron:357347435728863233> - ${row.iron}\n:gem: - ${row.diamond}`);
+			theEmbed.setTimestamp();
+			theEmbed.setColor(0x7289da);
+			if(msg.content.trim() == "=kingdominfo DM") {
+				try {
+					msg.author.dmChannel.send({embed: theEmbed});
+				}
+				catch (e) {
+					try {
+						msg.author.createDM().then(() => msg.author.dmChannel.send({embed: theEmbed}));
+					}
+					catch(e) {
+						msg.reply("To use this, please allow DMs from KingdomBot. " + e.message);
+					}
+				}
+				return;
+			}
+			msg.channel.send({embed: theEmbed});
+		}).catch(() => msg.reply("Couldn't find your kingdom... "));
+	}
+	if(msg.content.trim() === "=collect") {
+		sql.get('SELECT * FROM kingdoms WHERE userId = ? AND serverId = ?', [msg.author.id.toString(), msg.guild.id.toString()]).then(row => {
+			if(!row) {
+				msg.reply("Couldn't find your kingdom...");
+				return;
+			}
+			sql.get('SELECT * FROM timedEvents WHERE userId = ? AND serverId = ?', [msg.author.id.toString(), msg.guild.id.toString()]).then(row2 => {
+				if(!row2) {
+					msg.reply("I'm sorry, but there was an error processing your command.");
 					return;
 				}
-				var theEmbed = new Discord.RichEmbed();
-				theEmbed.setFooter("Requested by " + msg.author.username + "#" + msg.author.discriminator, msg.author.avatarURL);
-				theEmbed.setTitle("Kingdom Info");
-				theEmbed.setDescription(`Kingdom name: ${row.name}\nKingCoins: ${row.kingCoins}\n<:wood:356908039951089674> - ${row.wood}\n<:iron:357347435728863233> - ${row.iron}\n:gem: - ${row.diamond}`);
-				theEmbed.setTimestamp();
-				theEmbed.setColor(0x7289da);
-				msg.channel.send({embed: theEmbed});
-			}).catch(() => msg.reply("Couldn't find your kingdom... "));
-		}
+				var dateRightNow = new Date();
+				var remain = (dateRightNow - parseInt(row2.dateStarted)) % 1440000;
+				var coinsAdded = ((dateRightNow - parseInt(row2.dateStarted)) - remain) / 1440000;
+				if(coinsAdded > 60) {
+					coinsAdded = 60;
+					remain = 0;
+				}
+				if(coinsAdded == 0) {
+					msg.reply("You have no coins to collect right now. Try again later?");
+					return;
+				}
+				sql.run('UPDATE kingdoms SET copperCoins = ? WHERE userId = ? AND serverId = ?', [coinsAdded + parseInt(row.copperCoins, 10), msg.author.id.toString(), msg.guild.id.toString()]).then(() => msg.reply("You collected " + coinsAdded + " copper coin" + (coinsAdded - 1 ? "s" : "") + "!"));
+				sql.run('UPDATE timedEvents SET dateStarted = ? WHERE userId = ? AND serverId = ?', [dateRightNow - remain, msg.author.id.toString(), msg.guild.id.toString()]);
+			});
+		}).catch(() => msg.reply("Couldn't find your kingdom... "));
 	}
 	if(msg.content.trim().startsWith("=land")) {
-		sql.get('SELECT * FROM kingdoms WHERE userId = ? AND serverId = ?', [msg.author.id, msg.guild.id]).then(row => {
+		sql.get('SELECT * FROM kingdoms WHERE userId = ? AND serverId = ?', [msg.author.id.toString(), msg.guild.id.toString()]).then(row => {
 			if(!row) {
 				msg.reply("Couldn't find your kingdom...");
 				return;
